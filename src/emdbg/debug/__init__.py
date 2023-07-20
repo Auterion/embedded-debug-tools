61220526487d4d3a713e357121d066598cd9f87e
Niklas Hauser <niklas@auterion.com>
2023-07-20 13:28:18 +0200

    [docs] Link debug probes into docs

--- src/emdbg/debug/__init__.py
+++ src/emdbg/debug/__init__.py
@@ -5,8 +5,17 @@
 # Debug Tools
 
 This module manages the configuration and lifetime of debuggers and debug probes.
-In particular, it orchestrates the setup of J-Link and GDB and provides
+In particular, it orchestrates the setup of debug probes and GDB and provides
 scripting access to the GBD Python API and extends it with custom commands.
+
+Several debug probes are supported:
+
+- JLink via `emdbg.debug.jlink`.
+- OpenOCD via `emdbg.debug.openocd`.
+- CrashDebug via `emdbg.debug.crashdebug`.
+
+The debug probes have been tested with the JLink EDU mini, JLink BASE compact,
+and STLinkv3-MINIE.
 """
 
 from . import jlink
